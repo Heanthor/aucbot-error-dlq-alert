a3c6fe7fc7728cd4de0954c132aa0574390b402f
Reed Trevelyan <reedtrevelyan@gmail.com>
2022-08-28 12:16:28 -0400

    update message format

--- functions/snsConsume.py
+++ functions/snsConsume.py
@@ -8,6 +8,41 @@ import asyncio
 
 import os
 
+CONSOLE_URL = "https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#alarmsV2:alarm/%s?"
+"""
+Sample "Message" key:
+{
+  "AlarmName": "aucbot-pricealert-undelivered-messages",
+  "AlarmDescription": null,
+  "AWSAccountId": "533639758947",
+  "AlarmConfigurationUpdatedTimestamp": "2022-08-28T15:38:30.249+0000",
+  "NewStateValue": "ALARM",
+  "NewStateReason": "Threshold Crossed: 1 out of the last 1 datapoints [0.0 (28/08/22 15:34:00)] was greater than or equal to the threshold (0.0) (minimum 1 datapoint for OK -> ALARM transition).",
+  "StateChangeTime": "2022-08-28T15:39:16.518+0000",
+  "Region": "US East (N. Virginia)",
+  "AlarmArn": "arn:aws:cloudwatch:us-east-1:533639758947:alarm:aucbot-pricealert-undelivered-messages",
+  "OldStateValue": "OK",
+  "OKActions": [],
+  "AlarmActions": ["arn:aws:sns:us-east-1:533639758947:aucbot-prod-dlq-2"],
+  "InsufficientDataActions": [],
+  "Trigger": {
+    "MetricName": "ApproximateNumberOfMessagesNotVisible",
+    "Namespace": "AWS/SQS",
+    "StatisticType": "Statistic",
+    "Statistic": "AVERAGE",
+    "Unit": null,
+    "Dimensions": [{ "value": "ADSScan.fifo", "name": "QueueName" }],
+    "Period": 300,
+    "EvaluationPeriods": 1,
+    "DatapointsToAlarm": 1,
+    "ComparisonOperator": "GreaterThanOrEqualToThreshold",
+    "Threshold": 0.0,
+    "TreatMissingData": "missing",
+    "EvaluateLowSampleCountPercentile": ""
+  }
+}
+"""
+
 def handler(event, context):
     intents = discord.Intents.default()
 
@@ -27,11 +62,14 @@ class DiscordClient(discord.Client):
         self.group_id = int(os.getenv("ROLE_ID"))
 
     def build_message(self):
-        error_str = json.dumps(self.event, indent=4, sort_keys=True)
+        record = self.event["Records"][0]["Sns"]
+        message = json.loads(record['Message'])
+        msg = f"<@&{self.group_id}> **Aucbot error triggered:**\n"
+        msg += f"**Subject:** {record['Subject']}\n"
+        msg += f"**Metric:** {message['Trigger']['MetricName']}\n"
+        msg += f"**Detail:** ```{message['NewStateReason']}```\n"
         
-        msg = f"<@&{self.group_id}> - Aucbot error triggered:\n```"
-        msg += error_str
-        msg += "```"
+        msg += f"View alarm: {CONSOLE_URL % message['AlarmName']}"
 
         return msg
 
